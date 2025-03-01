9bffef94ff8a1bf5fa1af8427aad2ca0e8c737df
Jaroslaw <jaroslaw.chaikin@gmail.com>
2025-03-01 21:42:45 +0600

    small update header

--- src/pages/home/ui/Header/Header.jsx
+++ src/pages/home/ui/Header/Header.jsx
@@ -5,8 +5,9 @@ export const Header = ({ children }) => {
     <header className="h-[90px] w-screen p-2">
       <Container>
         <div className="flex justify-center">
-          <div className="flex gap-3 w-fit h-full p-4 px-10 rounded-xl bg-linear-to-r from-white/20 to-white/10 backdrop-blur-md ">
-            {children}
+          <div className="flex items-center w-fit h-full p-4 px-10 rounded-xl bg-linear-to-r from-white/20 to-white/10 backdrop-blur-md ">
+            <div className="mr-40 text-3xl font-bold">Bunker Generator</div>
+            <div className="flex gap-3">{children}</div>
           </div>
         </div>
       </Container>
